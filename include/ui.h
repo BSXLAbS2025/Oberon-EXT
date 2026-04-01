f2e6695388cf4c2ac47d98a29af55ff1773c22b4
BSX LABS <156119164+BSXLAbS2025@users.noreply.github.com>
2026-04-01 17:52:19 +0300

    Update ui.h

--- include/ui.h
+++ include/ui.h
@@ -1,5 +1,5 @@
-#ifndef UI_EXT_H
-#define UI_EXT_H
+#ifndef UI_H
+#define UI_H
 
 #ifdef _WIN32
     #include <curses.h>
@@ -7,24 +7,21 @@
     #include <ncurses.h>
 #endif
 
-#include <pthread.h>
-
 typedef struct {
-    WINDOW *header;     // Статус: Target, Threads, Time
-    WINDOW *main_log;   // Бегущие логи
-    WINDOW *sidebar;    // System Laws (Open Ports)
-    WINDOW *input_bar;  // Командная строка oberon-ext >
+    WINDOW *header;
+    WINDOW *main_log;
+    WINDOW *sidebar;
+    WINDOW *input_bar;
 } ui_t;
 
-void ui_init();
-void ui_cleanup();
-void ui_log(const char* msg, int type); // 0-info, 1-warn, 2-crit
-void ui_add_system_law(int port, const char* info);
-void ui_set_target(const char* target);
-void ui_refresh_all();
-char* ui_get_input();
+// Делаем gui видимой для всех файлов
+extern ui_t gui;
 
-// Мьютекс для синхронизации печати из разных потоков (сканеров)
-extern pthread_mutex_t ui_mutex;
+void ui_init();
+void ui_refresh();
+void ui_set_target(const char* target, const char* ip);
+void ui_add_law(int port, const char* desc);
+void ui_log(const char* msg, int type); // 0-info, 1-warn, 2-crit
+void ui_cleanup();
 
 #endif
