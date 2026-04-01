29f1f109306aa9e52df05f5a3f17ab5b1f41c39f
BSX LABS <156119164+BSXLAbS2025@users.noreply.github.com>
2026-04-01 17:31:46 +0300

    Update oberon.h

--- include/oberon.h
+++ include/oberon.h
@@ -14,17 +14,10 @@
     #define sleep_ms(ms) Sleep(ms)
     #define close_socket closesocket
     typedef int socklen_t;
-    // Макрос для запуска потока на Windows
     #define THREAD_FUNC DWORD WINAPI
     #define THREAD_HANDLE HANDLE
-    #define LIB_HANDLE HMODULE
-    #define LOAD_LIB(path) LoadLibraryA(path)
-    #define GET_FUNC GetProcAddress
-    #define CLOSE_LIB FreeLibrary
-    #define LIB_EXT ".dll"
+    #define SOCKET_TYPE SOCKET
 #else
-    #include <dlfcn.h>
-    #include <dirent.h>
     #include <sys/time.h>
     #include <sys/socket.h>
     #include <arpa/inet.h>
@@ -34,34 +27,18 @@
     #define sleep_ms(ms) usleep((ms) * 1000)
     #define close_socket close
     typedef int SOCKET;
+    #define SOCKET_TYPE SOCKET
     #define INVALID_SOCKET -1
-    // Макрос для запуска потока на Linux/Mac
     #define THREAD_FUNC void*
     #define THREAD_HANDLE pthread_t
-    #define LIB_HANDLE void*
-    #define LOAD_LIB(path) dlopen(path, RTLD_LAZY)
-    #define GET_FUNC dlsym
-    #define CLOSE_LIB dlclose
-    #define LIB_EXT ".so"
 #endif
 
-typedef void (*module_run_func)(char*, int);
-
-// Цвета
-#define CLR_RESET  "\033[0m"
-#define CLR_GREEN  "\033[1;32m"
-#define CLR_CYAN   "\033[1;36m"
-#define CLR_RED    "\033[1;31m"
-#define CLR_YELLOW "\033[1;33m"
-
-// Структура задачи для потока
 typedef struct {
     char ip[64];
     int port;
     int delay;
 } scan_task_t;
 
-// Прототипы из core.c
 void init_networking();
 void cleanup_networking();
 char* resolve_host(char *hostname);
