3d85d7f6c660088b94984e9acb853f0206452213
BSX LABS <156119164+BSXLAbS2025@users.noreply.github.com>
2026-04-01 17:36:05 +0300

    Update ui_core.c

--- src/ui_core.c
+++ src/ui_core.c
@@ -1,76 +1,73 @@
-#ifdef _WIN32
-    #include <curses.h>
-#else
-    #include <ncurses.h>
-#endif
 #include "ui.h"
-#include <pthread.h>
 #include <unistd.h>
 
-typedef struct {
-    WINDOW *header;
-    WINDOW *log;
-    WINDOW *sidebar;
-    WINDOW *input;
-} ui_ctx_t;
-
-ui_ctx_t gui;
-pthread_mutex_t ui_lock = PTHREAD_MUTEX_INITIALIZER;
+ui_t gui;
+#ifdef _WIN32
+    CRITICAL_SECTION ui_lock;
+    #define LOCK_UI EnterCriticalSection(&ui_lock)
+    #define UNLOCK_UI LeaveCriticalSection(&ui_lock)
+#else
+    pthread_mutex_t ui_lock = PTHREAD_MUTEX_INITIALIZER;
+    #define LOCK_UI pthread_mutex_lock(&ui_lock)
+    #define UNLOCK_UI pthread_mutex_unlock(&ui_lock)
+#endif
 
 void ui_init() {
-    initscr(); start_color(); cbreak(); noecho();
-    keypad(stdscr, TRUE);
-    init_pair(1, COLOR_CYAN, COLOR_BLACK);   // Header
-    init_pair(2, COLOR_GREEN, COLOR_BLACK);  // Laws
-    init_pair(3, COLOR_YELLOW, COLOR_BLACK); // Prompt
-    
-    int my, mx; getmaxyx(stdscr, my, mx);
-    gui.header = newwin(3, mx, 0, 0);
-    gui.log    = newwin(my-6, mx-35, 3, 0);
-    gui.sidebar = newwin(my-6, 35, 3, mx-35);
-    gui.input  = newwin(3, mx, my-3, 0);
-    scrollok(gui.log, TRUE);
+#ifdef _WIN32
+    InitializeCriticalSection(&ui_lock);
+#endif
+    initscr(); start_color(); cbreak(); noecho(); keypad(stdscr, TRUE);
+    init_pair(1, COLOR_CYAN, COLOR_BLACK); 
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_RED, COLOR_BLACK);
+
+    int y, x; getmaxyx(stdscr, y, x);
+    gui.header = newwin(3, x, 0, 0);
+    gui.main_log = newwin(y - 7, x - 40, 3, 0);
+    gui.sidebar = newwin(y - 7, 40, 3, x - 40);
+    gui.input_bar = newwin(3, x, y - 3, 0);
+    scrollok(gui.main_log, TRUE);
     ui_refresh();
 }
 
 void ui_refresh() {
-    pthread_mutex_lock(&ui_lock);
+    LOCK_UI;
     box(gui.header, 0, 0); mvwprintw(gui.header, 0, 2, " OBERON-EXT v5.0 ");
-    box(gui.log, 0, 0);
+    box(gui.main_log, 0, 0);
     box(gui.sidebar, 0, 0); mvwprintw(gui.sidebar, 0, 2, " SYSTEM LAWS ");
-    box(gui.input, 0, 0);
-    wrefresh(gui.header); wrefresh(gui.log); wrefresh(gui.sidebar); wrefresh(gui.input);
-    pthread_mutex_unlock(&ui_lock);
+    box(gui.input_bar, 0, 0);
+    wrefresh(gui.header); wrefresh(gui.main_log);
+    wrefresh(gui.sidebar); wrefresh(gui.input_bar);
+    UNLOCK_UI;
 }
 
-void ui_target_anim(const char* target) {
-    pthread_mutex_lock(&ui_lock);
-    for(int i=0; i<10; i++) {
-        mvwprintw(gui.header, 1, 2, "LOCKING TARGET: [%s] %c", target, "|/-\\"[i%4]);
-        wrefresh(gui.header); usleep(60000);
-    }
+void ui_set_target(const char* target, const char* ip) {
+    LOCK_UI;
+    wclear(gui.header); box(gui.header, 0, 0);
     wattron(gui.header, COLOR_PAIR(1) | A_BOLD);
-    mvwprintw(gui.header, 1, 2, "TARGET LOCKED: [%s]          ", target);
+    mvwprintw(gui.header, 1, 2, "TARGET LOCKED: %s (%s)", target, ip);
     wattroff(gui.header, COLOR_PAIR(1) | A_BOLD);
     wrefresh(gui.header);
-    pthread_mutex_unlock(&ui_lock);
+    UNLOCK_UI;
 }
 
-void ui_add_law(int port, const char* info) {
-    pthread_mutex_lock(&ui_lock);
+void ui_add_law(int port, const char* desc) {
+    LOCK_UI;
     static int line = 1;
     wattron(gui.sidebar, COLOR_PAIR(2));
-    mvwprintw(gui.sidebar, line++, 1, "-> PORT %-5d | %s", port, info);
+    mvwprintw(gui.sidebar, line++, 1, "LAW: PORT %d", port);
+    mvwprintw(gui.sidebar, line++, 2, " > %s", desc);
     wattroff(gui.sidebar, COLOR_PAIR(2));
-    if(line >= getmaxy(gui.sidebar)-2) line = 1;
     wrefresh(gui.sidebar);
-    pthread_mutex_unlock(&ui_lock);
+    UNLOCK_UI;
 }
 
 void ui_log(const char* msg) {
-    pthread_mutex_lock(&ui_lock);
-    wprintw(gui.log, " [*] %s\n", msg);
-    box(gui.log, 0, 0);
-    wrefresh(gui.log);
-    pthread_mutex_unlock(&ui_lock);
+    LOCK_UI;
+    wprintw(gui.main_log, " [*] %s\n", msg);
+    box(gui.main_log, 0, 0);
+    wrefresh(gui.main_log);
+    UNLOCK_UI;
 }
+
+void ui_cleanup() { endwin(); }
