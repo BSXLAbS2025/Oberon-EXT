f94ce8905fe53884f33d3ed50b7a9a2aacc4b7f2
BSX LABS <156119164+BSXLAbS2025@users.noreply.github.com>
2026-04-01 17:32:09 +0300

    Update ui_core.c

--- src/ui_core.c
+++ src/ui_core.c
@@ -1,108 +1,76 @@
+#ifdef _WIN32
+    #include <curses.h>
+#else
+    #include <ncurses.h>
+#endif
 #include "ui.h"
-#include <string.h>
+#include <pthread.h>
 #include <unistd.h>
 
-ui_t gui;
-pthread_mutex_t ui_mutex = PTHREAD_MUTEX_INITIALIZER;
+typedef struct {
+    WINDOW *header;
+    WINDOW *log;
+    WINDOW *sidebar;
+    WINDOW *input;
+} ui_ctx_t;
+
+ui_ctx_t gui;
+pthread_mutex_t ui_lock = PTHREAD_MUTEX_INITIALIZER;
 
 void ui_init() {
-    initscr();
-    start_color();
-    cbreak();
-    noecho();
+    initscr(); start_color(); cbreak(); noecho();
     keypad(stdscr, TRUE);
-
-    // Цветовые схемы
     init_pair(1, COLOR_CYAN, COLOR_BLACK);   // Header
-    init_pair(2, COLOR_GREEN, COLOR_BLACK);  // Success/Ports
-    init_pair(3, COLOR_RED, COLOR_BLACK);    // Critical/Alert
-    init_pair(4, COLOR_YELLOW, COLOR_BLACK); // Prompt
-
-    int max_y, max_x;
-    getmaxyx(stdscr, max_y, max_x);
-
-    // Создаем окна (y, x, start_y, start_x)
-    gui.header = newwin(3, max_x, 0, 0);
-    gui.main_log = newwin(max_y - 7, max_x - 30, 3, 0);
-    gui.sidebar = newwin(max_y - 7, 30, 3, max_x - 30);
-    gui.input_bar = newwin(3, max_x, max_y - 3, 0);
-
-    scrollok(gui.main_log, TRUE);
-    ui_refresh_all();
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);  // Laws
+    init_pair(3, COLOR_YELLOW, COLOR_BLACK); // Prompt
+    
+    int my, mx; getmaxyx(stdscr, my, mx);
+    gui.header = newwin(3, mx, 0, 0);
+    gui.log    = newwin(my-6, mx-35, 3, 0);
+    gui.sidebar = newwin(my-6, 35, 3, mx-35);
+    gui.input  = newwin(3, mx, my-3, 0);
+    scrollok(gui.log, TRUE);
+    ui_refresh();
 }
 
-void ui_set_target(const char* target) {
-    pthread_mutex_lock(&ui_mutex);
-    wclear(gui.header);
-    box(gui.header, 0, 0);
-    wattron(gui.header, COLOR_PAIR(1) | A_BOLD);
-    mvwprintw(gui.header, 1, 2, " TARGET LOCKED: [%s] ", target);
-    
-    // Анимация сканирования (Target Locked Effect)
-    const char* anim = "|/-\\";
-    for(int i = 0; i < 10; i++) {
-        mvwprintw(gui.header, 1, strlen(target) + 18, "[%c]", anim[i % 4]);
-        wrefresh(gui.header);
-        usleep(50000);
+void ui_refresh() {
+    pthread_mutex_lock(&ui_lock);
+    box(gui.header, 0, 0); mvwprintw(gui.header, 0, 2, " OBERON-EXT v5.0 ");
+    box(gui.log, 0, 0);
+    box(gui.sidebar, 0, 0); mvwprintw(gui.sidebar, 0, 2, " SYSTEM LAWS ");
+    box(gui.input, 0, 0);
+    wrefresh(gui.header); wrefresh(gui.log); wrefresh(gui.sidebar); wrefresh(gui.input);
+    pthread_mutex_unlock(&ui_lock);
+}
+
+void ui_target_anim(const char* target) {
+    pthread_mutex_lock(&ui_lock);
+    for(int i=0; i<10; i++) {
+        mvwprintw(gui.header, 1, 2, "LOCKING TARGET: [%s] %c", target, "|/-\\"[i%4]);
+        wrefresh(gui.header); usleep(60000);
     }
-    mvwprintw(gui.header, 1, strlen(target) + 18, "[ READY ]");
+    wattron(gui.header, COLOR_PAIR(1) | A_BOLD);
+    mvwprintw(gui.header, 1, 2, "TARGET LOCKED: [%s]          ", target);
     wattroff(gui.header, COLOR_PAIR(1) | A_BOLD);
     wrefresh(gui.header);
-    pthread_mutex_unlock(&ui_mutex);
+    pthread_mutex_unlock(&ui_lock);
 }
 
-void ui_add_system_law(int port, const char* info) {
-    pthread_mutex_lock(&ui_mutex);
+void ui_add_law(int port, const char* info) {
+    pthread_mutex_lock(&ui_lock);
     static int line = 1;
     wattron(gui.sidebar, COLOR_PAIR(2));
-    mvwprintw(gui.sidebar, line++, 1, "LAW #%d: PORT %d", line, port);
-    mvwprintw(gui.sidebar, line++, 1, "  -> %s", info);
-    box(gui.sidebar, 0, 0);
-    mvwprintw(gui.sidebar, 0, 2, " SYSTEM LAWS ");
+    mvwprintw(gui.sidebar, line++, 1, "-> PORT %-5d | %s", port, info);
     wattroff(gui.sidebar, COLOR_PAIR(2));
+    if(line >= getmaxy(gui.sidebar)-2) line = 1;
     wrefresh(gui.sidebar);
-    pthread_mutex_unlock(&ui_mutex);
+    pthread_mutex_unlock(&ui_lock);
 }
 
-const char* suggest_tool(int port) {
-    switch(port) {
-        case 21:   return "FTP: Try hydra brute-force";
-        case 22:   return "SSH: Check for libssh auth bypass";
-        case 80:   
-        case 443:  return "HTTP: Run gobuster / nikto";
-        case 445:  return "SMB: Vulnerable to EternalBlue?";
-        case 3389: return "RDP: Check for BlueKeep";
-        default:   return "Unknown: Manual recon required";
-    }
-}
-
-void ui_log(const char* msg, int type) {
-    pthread_mutex_lock(&ui_mutex);
-    if (type == 1) wattron(gui.main_log, COLOR_PAIR(2));
-    if (type == 2) wattron(gui.main_log, COLOR_PAIR(3));
-    
-    wprintw(gui.main_log, " [*] %s\n", msg);
-    
-    wattroff(gui.main_log, COLOR_PAIR(2) | COLOR_PAIR(3));
-    box(gui.main_log, 0, 0);
-    wrefresh(gui.main_log);
-    pthread_mutex_unlock(&ui_mutex);
-}
-
-void ui_refresh_all() {
-    box(gui.header, 0, 0);
-    mvwprintw(gui.header, 0, 2, " OBERON-EXT FRAMEWORK v5.0 ");
-    box(gui.main_log, 0, 0);
-    box(gui.sidebar, 0, 0);
-    mvwprintw(gui.sidebar, 0, 2, " SYSTEM LAWS ");
-    box(gui.input_bar, 0, 0);
-    
-    wrefresh(gui.header);
-    wrefresh(gui.main_log);
-    wrefresh(gui.sidebar);
-    wrefresh(gui.input_bar);
-}
-
-void ui_cleanup() {
-    endwin();
+void ui_log(const char* msg) {
+    pthread_mutex_lock(&ui_lock);
+    wprintw(gui.log, " [*] %s\n", msg);
+    box(gui.log, 0, 0);
+    wrefresh(gui.log);
+    pthread_mutex_unlock(&ui_lock);
 }
